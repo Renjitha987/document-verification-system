a7714dd0ea8ae5bbf01624b8c60ad2849a733e2f
Renjitha V.R <renjithavr18@gmail.com>
2026-07-24 10:27:41 +0530

    Fix Render settings

--- cert_verification_portal/settings.py
+++ cert_verification_portal/settings.py
@@ -20,13 +20,15 @@ load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-change-this-in-env')
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 #ALLOWED_HOSTS = [h.strip() for h in os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')]
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    ".vercel.app",
+    h.strip()
+    for h in os.getenv(
+        "ALLOWED_HOSTS",
+        "127.0.0.1,localhost,.vercel.app,.onrender.com"
+    ).split(",")
 ]
 
 # Application definition
